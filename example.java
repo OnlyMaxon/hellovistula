a77e61a112df615b2dbe267984ffc531e3413bf3
Jalil Orujli <79796416+OnlyMaxon@users.noreply.github.com>
2025-11-06 21:16:30 +0100

    Add CarWithLombok class with demo usage
    src

---
+++ example.java
@@ -0,0 +1,31 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CarWithLombok {
+    private String model;
+    private String brand;
+    private int year;
+    private int price;
+    private String color;
+    private int quantity;
+}
+
+class CarDemoWithLombok {
+    public static void main(String[] args) {
+        CarWithLombok car1 = new CarWithLombok("GLE", "Mercedes", 2020, 350_000, "white", 5);
+        CarWithLombok car2 = new CarWithLombok("GLC", "Mercedes", 2023, 200_000, "silver", 3);
+
+        System.out.println(car1);
+        System.out.println(car2);
+        System.out.println(car1.getColor());
+
+        int year = car1.getYear();
+        car1.setColor("black");
+
+        System.out.println("Are these objects equal? " + car1.equals(car2));
+        System.out.println("The HashCode of this object is: " + car2.hashCode());
+        System.out.println("The HashCode of this object is: " + car1.hashCode());
+    }
+}
